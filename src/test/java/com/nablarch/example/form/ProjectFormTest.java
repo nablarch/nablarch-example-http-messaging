ce531a4c3c46826655fafaa3370601b6e7004bd4
Takayuki Uchida <uchida.takayuki@tis.co.jp>
2023-08-25 21:21:34 +0900

    test: Formテスト追加

---
+++ src/test/java/com/nablarch/example/form/ProjectFormTest.java
@@ -0,0 +1,35 @@
+package com.nablarch.example.form;
+
+
+import nablarch.test.core.db.EntityTestSupport;
+import nablarch.test.junit5.extension.db.EntityTest;
+import org.junit.jupiter.api.Test;
+
+@EntityTest
+class ProjectFormTest {
+
+    private static final Class<?> targetClass = ProjectForm.class;
+
+    private EntityTestSupport support;
+
+    @Test
+    public void 文字列長と文字種の単項目精査結果が正しいことを検証する() {
+        String sheetName = "testCharsetAndLength";
+        String id = "charsetAndLength";
+        support.testValidateCharsetAndLength(targetClass, sheetName, id);
+    }
+
+    @Test
+    public void 文字列長と文字種以外の単項目精査結果が正しいことを検証する() {
+        String sheetName = "testSingleValidation";
+        String id = "singleValidation";
+        support.testSingleValidation(targetClass, sheetName, id);
+    }
+
+    @Test
+    public void 項目間精査結果が正しいことを検証する() {
+
+        support.testBeanValidation(targetClass, "testBeanValidation");
+    }
+
+}
